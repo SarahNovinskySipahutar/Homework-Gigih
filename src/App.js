7364e52e4953ac2038090f3adfaf74489045dae9
Sarah1911 <sarahnovinsky1911@gmail.com>
2022-03-28 01:57:21 +0700

    module 2 session 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Gif from '../src/components/index';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Playlist</h1>
+      <div className='track-list'>
+        {data.map((list)=>(
+          <Gif
+            key={list.id}
+            url={list.album.images[1].url}
+            nameAlbum={list.name}
+            nameArtist={list.artists[0].name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
+    
 export default App;
